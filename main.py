b978e7f8941c5dd02238fa01cd679c4a615a5ce6
Yuri Yoshinami <superpoodle4321@gmail.com>
2021-10-19 12:01:55 +0900

    Upgrade SQLite Database to PostgreSQL

--- main.py
+++ main.py
@@ -10,17 +10,20 @@ from flask_gravatar import Gravatar
 from forms import RegisterForm, LoginForm, CreatePostForm, CommentForm
 from functools import wraps
 from sqlalchemy import Table, Column, Integer, ForeignKey
+import os
 
 
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+# app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b' #sqlite
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY") #postgre
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'  #sqlite
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL") #postgre
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
